f8295397832922511d88f12990c3af6c6a9aa500
kadima <williamsvgw@gmail.com>
2025-04-12 23:33:02 +0100

    singlyLinkedList inner Node class implementation

---
+++ src/main/java/org/example/linkedLists/SinglyLinkedList.java
@@ -0,0 +1,23 @@
+package org.example.linkedLists;
+
+public class SinglyLinkedList<T> {
+  private static class Node<T> {
+    private T data;
+    private Node next;
+
+    public Node(T d, Node<T> n) {
+      data = d;
+      next = n;
+    }
+
+    public T getData(){
+      return data;
+    }
+    public Node<T> getNext(){
+      return next;
+    }
+    public void setNext(Node<T> n){
+      next = n;
+    }
+  }
+}
