9b72b728afcdfb9cbc6da5e463e18cdf62860479
kadima <williamsvgw@gmail.com>
2025-04-17 13:17:36 +0100

    circular linked list rotate method implemented

--- src/main/java/org/example/linkedLists/CircularLinkedList.java
+++ src/main/java/org/example/linkedLists/CircularLinkedList.java
@@ -8,6 +8,12 @@ public class CircularLinkedList<T> {
     return size;
   }
   public boolean isEmpty(){return size == 0;}
+
+  /**
+   * this returns the first node which must come after the tail
+   * or last node
+   * @return
+   */
   public T first(){
     if(isEmpty()) return null;
     return tail.getNext().getData();
@@ -16,5 +22,14 @@ public class CircularLinkedList<T> {
   public T last(){
     if(isEmpty()) return null;
     return tail.getData();
+  }
+
+  /**
+   * rotates the list one node clockwise
+   */
+  public void rotate(){
+    if(tail != null){
+      tail = tail.getNext();
+    }
   }
 }
